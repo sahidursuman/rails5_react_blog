6fc7aaf8196be591dc99d3ed74194fb6a590a8e6
okli <okli@MacBook-Pro-de-okli.local>
2017-05-15 22:18:25 +0200

    index posts

---
+++ app/javascript/packs/reducers/reducer_posts.js
@@ -0,0 +1,12 @@
+import _ from 'lodash';
+import { FETCH_POSTS, FETCH_POST, DELETE_POST } from '../actions';
+
+export default function(state = {}, action) {
+  switch (action.type) {
+  case FETCH_POSTS:
+    console.log(action.payload.data)
+    return _.mapKeys(action.payload.data.posts, 'id');
+  default:
+    return state;
+  }
+}
